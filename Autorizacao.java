b4c14ab4337dcedc8aac04d01b4e05a90d9c2e0f
rkacuta <rkacuta@gmail.com>
2017-05-16 01:20:59 -0300

    Atualização usando o padrão controller
    Atualização do código usando os padrões GRASP

--- Autorizacao.java
+++ Autorizacao.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//Uso do padrão controller para fazer as funções do caso de uso mostrar dados do cliente
 package segurado;
 
 /**
